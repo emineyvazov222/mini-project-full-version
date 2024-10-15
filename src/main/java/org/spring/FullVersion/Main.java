f82059a6520a5a86e084fa94984dd0d37bcdcc8e
Emin <eyvazovemin812@gmail.com>
2024-10-15 16:54:54 +0400

    add step3

--- src/main/java/org/spring/FullVersion/Main.java
+++ src/main/java/org/spring/FullVersion/Main.java
@@ -1,25 +1,37 @@
 package org.spring.FullVersion;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        String[] habits = {"eat", "drink", "sleep"};
-        Pet pet1 = new Pet("dog", "Rock", 5, 75, habits);
+        String[] arr = {"eat", "drink", "sleep"};
+        Pet pet1 = new Pet(Species.CAT, "Ares", 6, 80, arr);
         System.out.println(pet1);
-        pet1.eat();
-        pet1.foul();
-        pet1.respond();
+
+        Map<DayOfWeek, String> schedule = new HashMap<>();
+        schedule.put(DayOfWeek.MONDAY, "go to school");
+        schedule.put(DayOfWeek.TUESDAY, "play football");
+
+        Human father = new Human("Elsen", "Eyvazov", 1970, 90, schedule);
+        Human mother = new Human("Elnare", "Eyvazova", 1975, 85, schedule);
+
+        Family family = new Family(father, mother);
 
 
-        Human father = new Human("Vito", "Karleone", 1945);
-        Human mother = new Human("Jane", "Karleone", 1955);
-
-        String[][] schedule = {{"Monday", "go to school"}, {"Tuesday", "play football"}};
-        Human human1 = new Human("Michael", "Karleone", 1977, 90, mother, father, pet1, schedule);
-        System.out.println(human1);
-        human1.greetPet();
-        human1.describePet();
-        human1.feedPet(false);
+        Human child1 = new Human("Eltac", "Eyvazova", 1990);
+        Human child2 = new Human("Aytac", "Eyvazova", 1995);
+        family.addChild(child1);
+        family.addChild(child2);
 
 
+        family.setPet(pet1);
+        System.out.println(family);
+
+        System.out.println(family.countFamily());
+
+        family.deleteChild(0);
+        System.out.println(family.countFamily());
     }
+
 }
--- src/main/java/org/spring/FullVersion/Pet.java
+++ src/main/java/org/spring/FullVersion/Pet.java
@@ -1,42 +1,82 @@
 package org.spring.FullVersion;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Pet {
-    public String species;
-    public String nickname;
-    public int age;
-    public int trickLevel;
-    public String[] habits;
-
-
-
-    public void eat() {
-        System.out.println("I am eating");
-    }
-
-    public void respond() {
-        System.out.println("Hello, owner. I am - " + this.nickname + ". I miss you!");
-    }
-
-    public void foul() {
-        System.out.println("I need to cover it up");
-    }
+    private Species species;
+    private String nickname;
+    private int age;
+    private int trickLevel;
+    private String[] habits;
 
     public Pet() {
     }
 
-    public Pet(String species, String nickname) {
+    public Pet(Species species, String nickname) {
         this.species = species;
         this.nickname = nickname;
     }
 
-    public Pet(String species, String nickname, int age, int trickLevel, String[] habits) {
+    public Pet(Species species, String nickname, int age, int trickLevel, String[] habits) {
         this.species = species;
         this.nickname = nickname;
         this.age = age;
         this.trickLevel = trickLevel;
         this.habits = habits;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String[] getHabits() {
+        return habits;
+    }
+
+    public void setHabits(String[] habits) {
+        this.habits = habits;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public Species getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(Species species) {
+        this.species = species;
+    }
+
+    public int getTrickLevel() {
+        return trickLevel;
+    }
+
+    public void setTrickLevel(int trickLevel) {
+        this.trickLevel = trickLevel;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pet pet = (Pet) o;
+        return age == pet.age && trickLevel == pet.trickLevel && Objects.equals(species, pet.species) && Objects.equals(nickname, pet.nickname) && Objects.deepEquals(habits, pet.habits);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(species, nickname, age, trickLevel, Arrays.hashCode(habits));
     }
 
     @Override
@@ -47,7 +87,14 @@ public class Pet {
                 ", nickname='" + nickname + '\'' +
                 ", trickLevel=" + trickLevel +
                 ", habits=" + Arrays.toString(habits) +
+                ", canFly=" + species.canFly() +
+                ", numberOfLegs=" + species.getNumberOfLegs() +
+                ", hasFur=" + species.hasFur() +
                 '}';
     }
 
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
 }
