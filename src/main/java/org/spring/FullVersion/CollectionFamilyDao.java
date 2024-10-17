3b85a60bfdb7c175cfda0fa44a6d35b72d40d676
Emin <eyvazovemin812@gmail.com>
2024-10-17 14:42:36 +0400

    hw05 done

--- src/main/java/org/spring/FullVersion/CollectionFamilyDao.java
+++ src/main/java/org/spring/FullVersion/CollectionFamilyDao.java
@@ -5,26 +5,26 @@ import java.util.List;
 
 public class CollectionFamilyDao implements FamilyDao {
 
-    private List<Family> familyDatabase = new ArrayList<>();
+    private List<Family> families = new ArrayList<>();
 
 
     @Override
     public List<Family> getAllFamilies() {
-        return familyDatabase;
+        return families;
     }
 
     @Override
     public Family getFamilyByIndex(int index) {
-        if (index >= 0 && index < familyDatabase.size()) {
-            return familyDatabase.get(index);
+        if (index >= 0 && index < families.size()) {
+            return families.get(index);
         }
         return null;
     }
 
     @Override
     public boolean deleteFamily(int index) {
-        if (index >= 0 && index < familyDatabase.size()) {
-            familyDatabase.remove(index);
+        if (index >= 0 && index < families.size()) {
+            families.remove(index);
             return true;
         }
         return false;
@@ -32,17 +32,17 @@ public class CollectionFamilyDao implements FamilyDao {
 
     @Override
     public boolean deleteFamily(Family family) {
-        return familyDatabase.remove(family);
+        return families.remove(family);
     }
 
     @Override
     public void saveFamily(Family family) {
-        int index = familyDatabase.indexOf(family);
+        int index = families.indexOf(family);
 
         if (index >= 0) {
-            familyDatabase.set(index, family);
+            families.set(index, family);
         } else {
-            familyDatabase.add(family);
+            families.add(family);
         }
 
 
